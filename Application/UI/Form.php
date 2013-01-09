1256ead58e6545beadcb56032aa70e761925dc12
Tomáš Kolinger <tomas@kolinger.name>
2013-01-09 16:50:28 +0100

    added UI\Form

---
+++ Application/UI/Form.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of the Square CMS
+ *
+ * Copyright (c) 2011, 2012 Tomáš Kolinger <tomas@kolinger.name>
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Square\Application\UI;
+
+
+
+/**
+ * @author Tomáš Kolinger <tomas@kolinger.name>
+ */
+class Form extends \Nette\Application\UI\Form
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->setup();
+		$this->onSuccess[] = callback($this, 'process');
+	}
+
+
+
+	protected function setup()
+	{
+	}
+
+
+	/**
+	 * @param Form $form
+	 */
+	public function process(self $form)
+	{
+	}
+
+}
