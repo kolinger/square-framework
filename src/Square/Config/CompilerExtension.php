188d237ff90e62b3ac0e949649d05272e1b2283d
Tomáš Kolinger <tomas@kolinger.name>
2013-03-08 15:50:47 +0100

    added compiler extension

---
+++ src/Square/Config/CompilerExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the Square CMS
+ *
+ * Copyright (c) 2011, 2012 Tomáš Kolinger <tomas@kolinger.name>
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Square\Config;
+
+
+
+/**
+ * @author Tomáš Kolinger <tomas@kolinger.name>
+ */
+abstract class CompilerExtension extends \Nette\Config\CompilerExtension
+{
+
+	/**
+	 * Module name
+	 */
+	const NAME = NULL;
+
+}
